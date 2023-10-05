a148175ccc25941839fa6b6e035f4868f2b4aca8
Bruno <brunodlv111@gmail.com>
2023-10-05 02:31:39 -0300

    añadido superusuario beibeee

--- Notas_Web/AppNotas/admin.py
+++ Notas_Web/AppNotas/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import CrearNota, NotasGuardadas, EliminarNota
 
-# Register your models here.
+admin.site.register(CrearNota)
+admin.site.register(NotasGuardadas)
+admin.site.register(EliminarNota)
