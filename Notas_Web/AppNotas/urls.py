b01724b6b829933e6152b553f659b93df46c2bd3
Bruno <brunodlv111@gmail.com>
2023-10-05 23:40:03 -0300

    ultimo2

--- Notas_Web/AppNotas/urls.py
+++ Notas_Web/AppNotas/urls.py
@@ -1,6 +1,5 @@
 from django.urls import path
 from AppNotas import views
-from AppNotas import class_views
 urlpatterns = [
     path('', views.inicio, name='inicio'),
     path('crear_nota/', views.crear_nota, name='crear_nota'),
@@ -11,12 +10,3 @@ urlpatterns = [
 ]
 
 
-# URL's basadas en clases
-urlpatterns += [
-    path('class-list/', class_views.CrearNotaListView.as_view(), name="List"),
-    path('class-detail/<pk>/', class_views.CrearNotaDetailView.as_view(), name="Detail"),
-    path('class-create/', class_views.CrearNotaCreateView.as_view(), name="Create"),
-    path('class-update/<pk>/', class_views.CrearNotaUpdateView.as_view(), name="Update"),
-    path('class-delete/<pk>/', class_views.CrearNotaDeleteView.as_view(), name="Delete"),
-]
-# objects.get(pk=pk)
